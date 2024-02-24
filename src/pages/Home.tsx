671b9bfab7d06fb7036f9ec2e8daf163004bd06b
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-02-24 12:00:15 -0300

    style: created styles at page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,42 @@
-export const Home: React.FC = () => (
-    <>
-    <h1>tela de login</h1>
-    </>
-)
+import { useForm } from "react-hook-form"
+import { Box, Button, Container, FormControl, TextField } from "@mui/material"
+
+interface IFormInput {
+    firstName: string;
+    lastName: string;
+    age: number;
+}
+
+export const Home: React.FC = () => {
+
+    const { handleSubmit, register } = useForm<IFormInput>()
+
+    const handleForm = (data: IFormInput) => {
+
+        console.log(data)
+
+    }
+
+
+    return (
+        <Box sx={{ width: "100vw", minHeight: "100vh", background:"#292929" }}>
+            <Container>
+                <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
+                    <FormControl onSubmit={handleSubmit(handleForm)} sx={{ background: "#fff", padding: "70px", borderRadius:"10px" }}>
+                       
+                        <TextField id="firstName" label="Digite seu nome" variant="outlined" {...register("firstName")} placeholder="Digite seu nome" sx={{my:"15px"}}/>
+                        
+                        <TextField id="lastName" label="Digite seu nome" variant="outlined" {...register("lastName")} placeholder="Digite seu nome" sx={{my:"15px"}}/>
+                        
+                        <TextField id="age" label="Digite sua age" variant="outlined" {...register("age")} placeholder="Digite sua age" sx={{my:"15px"}}/>
+                       
+                        <Button type="submit" variant="contained">Enviar</Button>
+                    </FormControl>
+                </Box>
+            </Container>
+        </Box>
+
+    )
+}
+
+
