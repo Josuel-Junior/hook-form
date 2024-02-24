3e609a07ee3313c7d36362d038e9b6393582283d
Josuel Junior <108230374+Josuel-Junior@users.noreply.github.com>
2024-02-24 12:39:56 -0300

    feat: set errors at form

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # Getting Started with Create React App
-Tela de login com React Hook Form e Zod
+Tela de Cadastro com React Hook Form e Zod
 
 link: https://master.d3bhnpcd78c5zs.amplifyapp.com/
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useForm } from "react-hook-form"
-import { Box, Button, Container, FormControl, TextField } from "@mui/material"
+import { Box, Button, Container, FormControl, TextField, Typography } from "@mui/material"
 
 interface IFormInput {
     firstName: string;
@@ -9,29 +9,40 @@ interface IFormInput {
 
 export const Home: React.FC = () => {
 
-    const { handleSubmit, register } = useForm<IFormInput>()
+    const { handleSubmit, register, formState: { errors }, reset } = useForm<IFormInput>()
 
     const handleForm = (data: IFormInput) => {
 
         console.log(data)
 
+        console.log(errors.age)
+        reset()
+
     }
 
 
     return (
-        <Box sx={{ width: "100vw", minHeight: "100vh", background:"#292929" }}>
+        <Box sx={{ width: "100vw", minHeight: "100vh", background: "#292929" }}>
             <Container>
                 <Box sx={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", minHeight: "100vh" }}>
-                    <FormControl onSubmit={handleSubmit(handleForm)} sx={{ background: "#fff", padding: "70px", borderRadius:"10px" }}>
-                       
-                        <TextField id="firstName" label="Digite seu nome" variant="outlined" {...register("firstName")} placeholder="Digite seu nome" sx={{my:"15px"}}/>
-                        
-                        <TextField id="lastName" label="Digite seu nome" variant="outlined" {...register("lastName")} placeholder="Digite seu nome" sx={{my:"15px"}}/>
-                        
-                        <TextField id="age" label="Digite sua age" variant="outlined" {...register("age")} placeholder="Digite sua age" sx={{my:"15px"}}/>
-                       
-                        <Button type="submit" variant="contained">Enviar</Button>
-                    </FormControl>
+                    <form onSubmit={handleSubmit(handleForm)}>
+                        <FormControl sx={{ background: "#fff", padding: "70px", borderRadius: "10px" }}>
+                            <TextField id="firstName" label="Digite seu nome" variant="outlined" {...register("firstName", { required: true, minLength: 2 })} placeholder="Digite seu nome" sx={{ my: "15px" }} />
+                            {errors.firstName && (<Typography>
+                                Campo obrigatório
+                            </Typography>)}
+
+                            <TextField id="lastName" label="Digite seu sobrenome" variant="outlined" {...register("lastName", { required: true, minLength: 2 })} placeholder="Digite seu nome" sx={{ my: "15px" }} />
+                            {errors.lastName && (<Typography>
+                                Campo obrigatório
+                            </Typography>)}
+                            <TextField id="age" label="Digite sua age" variant="outlined" {...register("age", { required: true, min: 2 })} placeholder="Digite sua age" sx={{ my: "15px" }} />
+                            {errors.age && (<Typography>
+                                Campo obrigatório
+                            </Typography>)}
+                            <Button type="submit" variant="contained">Enviar</Button>
+                        </FormControl>
+                    </form>
                 </Box>
             </Container>
         </Box>
